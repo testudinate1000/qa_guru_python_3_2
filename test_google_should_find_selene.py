7b0b9be8c05b726e2eff59311869598292b4b983
testudinate1000 <olenakuznietsova@yandex.ru>
2022-11-27 12:12:16 +0300

    Clarify expected result

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -5,4 +5,4 @@ from selene import be, have
 def test_google_should_find_selene(open_browser):
     page = open_browser
     page.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    page.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    page.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
