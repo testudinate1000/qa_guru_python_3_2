c150a6f067ed80c3fe33c019d361dc2cc5bd8722
testudinate1000 <olenakuznietsova@yandex.ru>
2022-11-27 12:10:02 +0300

    Change fixture

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import pytest
 from selene.support.shared import SharedConfig, SharedBrowser
 
@@ -7,18 +7,6 @@ BASE_URL = 'https://google.com'
 
 @pytest.fixture
 def open_browser():
-    config = SharedConfig(window_width=1600, window_height=600)
-    browser = SharedBrowser(config)
-    return browser.open(BASE_URL)
-=======
-import pytest
-from selene.support.shared import SharedConfig, SharedBrowser
-
-BASE_URL = 'https://google.com'
-
-
-@pytest.fixture
-def open_browser():
-    config = SharedConfig(window_width=1600, window_height=600)
+    config = SharedConfig(window_width=1920, window_height=1080)
     browser = SharedBrowser(config)
     return browser.open(BASE_URL)
